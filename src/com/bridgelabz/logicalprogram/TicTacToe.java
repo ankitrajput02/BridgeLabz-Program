0a5535dfcabb15dff1b99f377125d085a78df60b
explorehorizon15 <explorehorizon15@gmail.com>
2020-01-18 14:21:55 +0530

    Update Tic Tac

--- src/com/bridgelabz/logicalprogram/TicTacToe.java
+++ src/com/bridgelabz/logicalprogram/TicTacToe.java
@@ -1,38 +1,95 @@
 package com.bridgelabz.logicalprogram;
-
 import com.bridgelabz.utility.Utility;
 
 public class TicTacToe {
-	public void printDesk(char[][] gameDesk) {
-		System.out.println("Before Play");
-		for(int i=0;i<5;i++) {
-			for(int j=0;j<5;j++) {
-				System.out.print(gameDesk[i][j]+" ");
-			}
-			System.out.println();
-		}
-		}
-
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		char[][] gameDesk= {{' ','|',' ','|',' '},
-							{'-','+','-','+','-'},
-							{' ','|',' ','|',' '},
-							{'-','+','-','+','-'},
-							{' ','|',' ','|',' '}};
-	//Before play
-	TicTacToe t=new TicTacToe();
-	t.printDesk(gameDesk);
-	System.out.println("Select Choice 'X' or 'O'");
-	char ch=Utility.inputChar();
-	char symbol=' ';
-	if(ch=='X'||ch=='x') {
-		symbol='X';
+				{'-','|','-','|','-'},
+				{' ','|',' ','|',' '},
+				{'-','|','-','|','-'},
+				{' ','|',' ','|',' '}};
+		//Before play
+		char arr[]=new char[10];
+		
 	}
-	int count=0;
-	while(count<9) {
+
+
+
+
+
+
+
+	//		while(true) {
+	//		int num=9;int pos=0;
+	//		while(num>=0) {
+	//			if(num%2==0) {
+	//				num--;
+	//				int distinct=0;
+	//				
+	//				while(distinct<10) {
+	//					pos=(int)(Math.random()*9);
+	//					if(!newPos[pos] ) {
+	//						System.out.println(pos);
+	//						placeSymbol(gameDesk,pos,'p');
+	//						printDesk(gameDesk);
+	//						distinct++;
+	//						//newPos[pos]=true;
+	//					}
+	//				}
+	//				
+	//			}	
+	//			else {
+	//				System.out.println("Enter Position");
+	//				pos=Utility.inputNumber();
+	//				placeSymbol(gameDesk,pos,'u');
+	//				printDesk(gameDesk);
+	//				num--;
+	//			}
+	//		}
+	//	}
+
+public static void printDesk(char[][] gameDesk) {
+	//System.out.println("Before Play");
+	for(int i=0;i<gameDesk.length;i++) {
+		for(int j=0;j<gameDesk.length;j++) {
+			System.out.print(gameDesk[i][j]+" ");
+		}
+		System.out.println();
+	}
+}
+public static void userFill(char[][] gameDesk,char[] arr) {
 	System.out.println("Enter Position");
 	int pos=Utility.inputNumber();
+	if(arr[pos]!='f') {
+		placeSymbol(gameDesk,pos,'u');
+		printDesk(gameDesk);
+		arr[pos]='f';
+	}
+	else {
+		userFill(gameDesk, arr);
+	}
+}
+public static void systemFill(char[][] gameDesk,char[] arr) {
+	int pos=(int)(Math.random()*9);
+	if(arr[pos]!='f') {
+		placeSymbol(gameDesk,pos,'p');
+		printDesk(gameDesk);
+		arr[pos]='f';
+	}
+	else {
+		systemFill(gameDesk, arr);
+	}
+}
+
+public static void placeSymbol(char[][] gameDesk,int pos,char user) {
+	char symbol =' ';
+	if(user=='u') {
+		symbol='X';
+	}
+	else if(user=='p'){
+		symbol='O';
+	}
 	switch(pos) {
 	case 1:
 		gameDesk[0][0]=symbol;
@@ -62,11 +119,8 @@ public class TicTacToe {
 		gameDesk[4][4]=symbol;
 		break;
 	default:
-		System.out.println("Enter valid position");
 		break;
 	}
-	t.printDesk(gameDesk);
-	count++;
-	}
-	}
+
+}
 }
