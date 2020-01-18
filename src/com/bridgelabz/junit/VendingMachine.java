93ea7c9945d2553953807b2011fe60fdd820e79f
explorehorizon15 <explorehorizon15@gmail.com>
2020-01-18 17:29:11 +0530

    Add Vending Machine

--- src/com/bridgelabz/junit/VendingMachine.java
+++ src/com/bridgelabz/junit/VendingMachine.java
@@ -8,10 +8,7 @@ import com.bridgelabz.utility.Utility;
  * Desc ->There is 1, 2, 5, 10, 50, 100, 500 and 1000 Rs Notes which can be
  * returned by Vending Machine. Write a Program to calculate the minimum number
  * of Notes as well as the Notes to be returned by the Vending Machine as a Change
- * I/P -> read the Change in Rs to be returned by the Vending Machine
- * O/P -> Two Outputs - one the number of minimum Note needed to give the change 
- * and second list of Rs Notes that would given in the Change
- *
+ * 
  */
 public class VendingMachine {
 	public static void main(String[] args) {
@@ -23,26 +20,3 @@ public class VendingMachine {
 
 	
 }
-//	
-//	while(amount!=0) {
-//			for(int i=notes.length-1;i>=0;i--) {
-//				for(int j=)
-//				if(amount>=notes[i] && amount != 0) {
-//					noteCounts[i]=amount/notes[i];
-//					System.out.println(notes[i]+"X"+noteCounts[i]+"="+notes[i]*noteCounts[i]);
-//					int amount1=notes[i]*noteCounts[i];
-//					if(amount1==0) {
-//						break;
-//					}
-//					else {
-//						amount-=amount1;
-//						//System.out.println("Remains "+amount);
-//						calculateNotes(amount,notes,noteCounts);
-//					}
-//				}
-//				else if(amount>0){
-//					i--;
-//				}
-//		
-//			}
-//		}
